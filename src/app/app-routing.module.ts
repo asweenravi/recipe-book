77e89b525aa9f0ab222bf25e54828d1a57aba0ff
asweenravi <asweenravi@gmail.com>
2019-08-08 07:28:11 +0530

    component creation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RecipeBookComponent } from './states/recipe-book/recipe-book.component';
+import { ShoppingComponent } from './states/shopping/shopping.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: RecipeBookComponent},
+  {path: 'home', component: RecipeBookComponent},
+  {path: 'shopping', component: ShoppingComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RecipeListingComponent } from './components/recipe-listing/recipe-listing.component';
+import { RecipeDetailsComponent } from './components/recipe-details/recipe-details.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ShoppingListComponent } from './components/shopping-list/shopping-list.component';
+import { RecipeBookComponent } from './states/recipe-book/recipe-book.component';
+import { ShoppingComponent } from './states/shopping/shopping.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipeListingComponent,
+    RecipeDetailsComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    RecipeBookComponent,
+    ShoppingComponent
   ],
   imports: [
     BrowserModule,
