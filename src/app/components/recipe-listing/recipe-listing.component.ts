33ad63325fdbbdcd86bf9719e9eee182041013b0
asweenravi <asweenravi@gmail.com>
2019-08-08 14:34:49 +0530

    UI for Recipe book

--- src/app/components/recipe-listing/recipe-listing.component.ts
+++ src/app/components/recipe-listing/recipe-listing.component.ts
@@ -7,6 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListingComponent implements OnInit {
 
+  recipes: any = [
+    {
+      title: 'Lamb kebab burger',
+      description: 'Jazz up a shop-bought lamb burger with garlic mayo, gherkins and chilli for a hot and spicy twist. Perfect for lazy summer weekends',
+      imageURL: 'https://www.bbcgoodfood.com/sites/default/files/styles/recipe/public/recipe/recipe-image/2019/05/hot-and-spicy-burgers.jpg?itok=Ao-88qqa'
+    },
+    {
+      title: 'Chicken & mushroom spud pies',
+      description: 'Jazz up a jacket potato with this creamy filling',
+      imageURL: 'https://www.bbcgoodfood.com/sites/default/files/styles/recipe/public/recipe_images/recipe-image-legacy-id--792455_11.jpg?itok=wRvrD2ir'
+    }
+  ]
+
   constructor() { }
 
   ngOnInit() {
