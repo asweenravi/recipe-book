25b52b14fa2dd2edb4d420791da81689adc04822
asweenravi <asweenravi@gmail.com>
2019-08-08 11:03:00 +0530

    Shoping list UI creation and form validations and clear functionality implementation

--- src/app/components/shopping-list/shopping-list.component.ts
+++ src/app/components/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-shopping-list',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingListComponent implements OnInit {
 
+  shoppingForm = new FormGroup({
+    name: new FormControl('',[Validators.required]),
+    amount: new FormControl('',[Validators.required])
+  });
+
+  cartItems: any = [
+    {
+      name: 'Apple',
+      amount: '5'
+    },
+    {
+      name: 'Orange',
+      amount: '6'
+    },
+    {
+      name: 'Fig',
+      amount: '2'
+    },
+  ]
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  revert() {
+    this.shoppingForm.reset()
   }
 
 }
