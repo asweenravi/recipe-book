87d17ba7a9478031fdd68bbcfa74555e1665935f
asweenravi <asweenravi@gmail.com>
2019-08-09 07:39:18 +0530

    adding path to details page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RecipeBookComponent } from './states/recipe-book/recipe-book.component';
 import { ShoppingComponent } from './states/shopping/shopping.component';
+import { RecipeDetailsComponent } from './components/recipe-details/recipe-details.component';
 
 
 const routes: Routes = [
   {path: '', component: RecipeBookComponent},
-  {path: 'home', component: RecipeBookComponent},
-  {path: 'shopping', component: ShoppingComponent}
+  {path: 'recipe', component: RecipeBookComponent},
+  {path: 'shopping', component: ShoppingComponent},
+  {path: 'recipe/:id', component: RecipeDetailsComponent}
 ];
 
 @NgModule({
--- src/app/components/recipe-listing/recipe-listing.component.ts
+++ src/app/components/recipe-listing/recipe-listing.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-listing',
@@ -11,11 +12,13 @@ export class RecipeListingComponent implements OnInit {
 
   recipes: any = [
     {
+      id: 1,
       title: 'Lamb kebab burger',
       description: 'Jazz up a shop-bought lamb burger with garlic mayo, gherkins and chilli for a hot and spicy twist. Perfect for lazy summer weekends',
       imageURL: 'https://www.bbcgoodfood.com/sites/default/files/styles/recipe/public/recipe/recipe-image/2019/05/hot-and-spicy-burgers.jpg?itok=Ao-88qqa'
     },
     {
+      id: 2,
       title: 'Chicken & mushroom spud pies',
       description: 'Jazz up a jacket potato with this creamy filling',
       imageURL: 'https://www.bbcgoodfood.com/sites/default/files/styles/recipe/public/recipe_images/recipe-image-legacy-id--792455_11.jpg?itok=wRvrD2ir'
@@ -34,7 +37,7 @@ export class RecipeListingComponent implements OnInit {
     description: new FormControl('',[Validators.required])
   });
 
-  constructor() { }
+  constructor( private router: Router) { }
 
   ngOnInit() {
   }
@@ -44,11 +47,16 @@ export class RecipeListingComponent implements OnInit {
   }
 
   addRecipe() {
+    this.recipe['id'] = this.recipes.length + 1;
     this.recipe['title'] = this.recipeForm.controls.title.value;
     this.recipe['imageURL'] = this.recipeForm.controls.imageURL.value;
     this.recipe['description'] = this.recipeForm.controls.description.value;
     this.recipes.push(this.recipe);
     this.revert();
+  }
+
+  openDetails(id) {
+    this.router.navigate(['recipe', id])
   }
 
 }
