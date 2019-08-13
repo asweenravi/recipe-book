d986264415dee09b6333c9f24c8e5878fe570da4
asweenravi <asweenravi@gmail.com>
2019-08-13 15:03:31 +0530

    added ingredients in the recipe details page

--- src/app/components/recipe-details/recipe-details.component.ts
+++ src/app/components/recipe-details/recipe-details.component.ts
@@ -8,13 +8,17 @@ import { Component, OnInit } from '@angular/core';
 export class RecipeDetailsComponent implements OnInit {
 
   recipe = {
-    id: 1,
-    title: 'Lamb kebab burger',
-    description: 'Jazz up a shop-bought lamb burger with garlic mayo, gherkins and chilli for a hot and spicy twist. Perfect for lazy summer weekends',
-    imageURL: 'https://www.bbcgoodfood.com/sites/default/files/styles/recipe/public/recipe/recipe-image/2019/05/hot-and-spicy-burgers.jpg?itok=Ao-88qqa'
+    id: 2,
+    title: 'Chicken & mushroom spud pies',
+    description: 'Jazz up a jacket potato with this creamy filling',
+    imageURL: 'https://www.bbcgoodfood.com/sites/default/files/styles/recipe/public/recipe_images/recipe-image-legacy-id--792455_11.jpg?itok=wRvrD2ir',
+    ingredients: ['lamb','buns']
   }
+  editable: boolean;
 
-  constructor() { }
+  constructor() { 
+    this.editable = false;
+  }
 
   ngOnInit() {
   }
